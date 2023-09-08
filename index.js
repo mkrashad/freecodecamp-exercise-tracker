b86984677dcca7e4a89877a0a0f4e670671af2a8
Rashad Mahmudov <mkrashad25@gmail.com>
2023-09-08 16:27:38 +0200

    Complete 6 points

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 const express = require('express');
+const bodyparser = require('body-parser');
 const app = express();
 const cors = require('cors');
+const userService = require('./services/user.js');
 require('dotenv').config();
+require('./db/db');
 
 app.use(cors());
 app.use(express.static('public'));
+
+// Body-parser middleware
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.post('/api/users', async function (req, res) {
+  const { username } = req.body;
+  const result = await userService.addUser(username);
+  res.json(result);
+});
 
-
-
+app.get('/api/users', async function (req, res) {
+  const users = await userService.getUsers();
+  res.json(users);
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port);
---
+++ services/user.js
@@ -0,0 +1,23 @@
+let UserModel = require('../model/user');
+
+const addUser = (username) => {
+  const msg = new UserModel({
+    username,
+  });
+  msg
+    .save()
+    .then((_) => {
+      console.log('Data successfully added');
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+  return msg;
+};
+
+const getUsers = () => {
+  const users = UserModel.find().exec();
+  return users
+};
+
+module.exports = { addUser, getUsers };
